de04ae24b2fe32c9f55e252bce26720a468e58f4
unknown <bandrock2806@gmail.com>
2023-05-05 18:31:49 -0400

    Material Calculo numerico

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,24 +1,31 @@
 from math import  *
 #Nombre: Ricardo Balducci.
 #Cedula: 28.308.177
+
+def f(x):
+    return e**x-3*x**2 #  sin(x) - e**-x 
+
+def df(x):
+    return e**x-6*x #  cos(x) + e**-x
+
 def newthon_raphson(f, df, x0, tol, max_iter=5):
     i = 0
+    Error_a = 100
     print("\nMétodo de Newton-Raphson\n")
-    while i < max_iter:
+    print ("| i | xi | xi+1 |")
+    print ("----------------------")
+    while (i < max_iter and Error_a > tol):
         x1 = x0 - f(x0) / df(x0)
+        print ('|',i, '|', x0, '|','|',x1, '|' )
         if abs(x1 - x0) < tol * abs(x1):
             return x1
         x0 = x1
         i += 1
+    print('\nEs la mejor solución aproximada se encuentra en m(',i-1,') =',x1,'\n')    
     raise ValueError("El método de Newton-Raphson no converge después de %d iteraciones." % max_iter)#en caso de no poder
 
-def f(x):
-    return sin(x) - e**-x # e**x-3*x**2
-
-def df(x):
-    return cos(x) + e**-x #e**x-6*x 
-
-x0 = 0.5  # valor inicial de x
-tol = 0.02 # margen de error
-raiz = newthon_raphson(f, df, x0, tol)  # encontramos la raíz de f
-print("La mejor solución aproximacion es: ",raiz)  # mostramos mejor solución aproximacion
+if __name__ == '__main__':
+    x0 = 0.7  # valor inicial de x
+    tol = 0.02 # margen de error
+    raiz = newthon_raphson(f, df, x0, tol)  # encontramos la raíz de f
+    print('\nfin')
